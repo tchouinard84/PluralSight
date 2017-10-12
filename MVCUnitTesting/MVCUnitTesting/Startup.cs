f0c51e23974eace2ef371ea6320dadcecd56dbaa
tchouina <timothy.chouinard@emp-corp.com>
2017-10-12 08:31:08 -0400

    ASP.NET MVC Testing From Scratch : Testing The Controller - Initial Commit (created a new ASP.NET MVC web application with Unit Tests).

---
+++ MVCUnitTesting/MVCUnitTesting/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MVCUnitTesting.Startup))]
+namespace MVCUnitTesting
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
